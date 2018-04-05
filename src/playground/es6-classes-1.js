4f8c7dda975490cc63b29b2b6f5b3365e618fcc0
caplincapture <leuping@gmail.com>
2018-04-05 12:41:53 -0700

    es6 class extensions

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,8 +12,43 @@ class Person {
     }
 }
 
-const me = new Person('Gabriel Leupin', 29);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    
+    getDescription() {
+        let description = super.getDescription();
+        
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        
+        return description;
+    }
+}
 
-const other = new Person();
-console.log(other.getDescription());
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (this.homeLocation) {
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Traveler('Gabriel Leupin', 29, 'Paris');
+console.log(me.getGreeting());
+
+const other = new Traveler('Matt', 90187324590, 'Timbuktu');
+console.log(other);
